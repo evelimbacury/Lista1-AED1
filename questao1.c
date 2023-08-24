e01d587aeeeb68435133dd68090c33fd4f5f05a5
Gabrielviz765 <gabrielmoriz765@gmail.com>
2023-08-24 11:18:13 -0400

    se liga amor, o pai ja fez uma ghihihihihih

--- questao1.c
+++ questao1.c
@@ -1,5 +1,28 @@
-#include <stdio.h>
+#include<stdio.h>
+
+int fibonacci(int i, int a, int b){
+    int somatorio;
+    if (i == 10)
+    {
+        return 1;
+    }
+    imprintmir(a);
+    somatorio = a +b;
+    a = b;
+    b = somatorio;
+
+
+    
+    fibonacci(i+1,a,b);
+}
+
+void imprintmir(int num){
+    printf("%d\n", num);
+}
 
 int main(){
-    
+   int i = 0, a = 1, b = 1; 
+
+    fibonacci(i,a,b);
+return 0;
 }
