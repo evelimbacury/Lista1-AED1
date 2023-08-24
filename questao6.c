192ef192804df864ad55cb70ab36b2d86b04a7fe
evelimbacury <evelimbacury@gmail.com>
2023-08-24 12:07:31 -0400

    Minhas alterações - parte 1

--- questao6.c
+++ questao6.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
-int main(){
+int contarNumRepetidos(int *vetor, int quant, int tam){
     
+}
+
+int main(){
+    int t = 5, vetor[t], quant = 0, i;
+
+    printf("Insira 5 números\n");
+
+    for(i = 0; i < t; i++){
+        printf("Num %d: ", i+1);
+        scanf("%d", &vetor[i]);
+    }
+
+    contarNumRepetidos(vetor, quant, t);
+    //printf(quant);
 }
