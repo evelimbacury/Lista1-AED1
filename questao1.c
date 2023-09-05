989a36eddefa0d16581f312c5ab2f220a0f7d4e8
Gabrielviz765 <gabrielmoriz765@gmail.com>
2023-09-04 20:13:40 -0400

    questão 1 SOCORROOOOOO

--- questao1.c
+++ questao1.c
@@ -3,29 +3,37 @@
 #include<stdio.h>
 
 
-int fibonacci(int i, int a, int b){
-    int somatorio;
-    if (i == 10)
-    {
-        return 1;
-    }
-    imprintmir(a);
-    somatorio = a +b;
-    a = b;
-    b = somatorio;
-
-
-    
-    fibonacci(i+1,a,b);
-}
-
 void imprintmir(int num){
     printf("%d\n", num);
 }
 
-int main(){
-   int i = 0, a = 1, b = 1; 
+int fibonacci(int i, int a, int b, int quantidadenumeros){
+    int novoSuc, soma = 0;
+    if (i == quantidadenumeros)
+    {
+        return 1;
+    }
 
-    fibonacci(i,a,b);
+    imprintmir(a);
+
+    novoSuc = a + b;
+    a = b;
+    b = novoSuc;
+
+    fibonacci(i+1,a,b, quantidadenumeros);
+    
+}
+
+
+
+int main(){
+   int i = 0, a = 1, b = 1, soma; 
+   int quantidadenumeros;
+   printf("informes quantos numeros queres");
+   scanf("%d", &quantidadenumeros);
+
+    soma = fibonacci(i, a, b, quantidadenumeros);
+
+    printf("\nA soma e %d", soma);
     return 0;
 }
